510c7648bf537ea18311cf4bbd11a209bc91231c
Alex <mist962@gmail.com>
2021-01-07 22:17:03 +0300

    Add some resources and data class

---
+++ app/src/main/java/com/sideki/imagesearchapp/UnsplashPhoto.kt
@@ -0,0 +1,30 @@
+package com.sideki.imagesearchapp
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class UnsplashPhoto(
+    var id: String,
+    var description: String?,
+    var urls: UnsplashPhotoUrls,
+    var user: UnsplashUser
+) : Parcelable {
+    
+    @Parcelize
+    data class UnsplashPhotoUrls(
+        var raw: String,
+        var full: String,
+        var regular: String,
+        var small: String,
+        var thumb: String
+    ) : Parcelable
+
+    @Parcelize
+    data class UnsplashUser(
+        var name: String,
+        var username: String
+    ) : Parcelable {
+        val attributionUrl get() = "https://unsplash.com/$username?utm_source=ImageSearchApp&utm_medium=referral"
+    }
+}
